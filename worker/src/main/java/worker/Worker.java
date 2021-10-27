dbf3bfb9562e9ea889396e5a248c897f209eb368
micavargas130 <mica.vargas2000@gmail.com>
2021-10-26 23:29:05 -0300

    prueba

--- worker/src/main/java/worker/Worker.java
+++ worker/src/main/java/worker/Worker.java
@@ -5,7 +5,7 @@ import redis.clients.jedis.exceptions.JedisConnectionException;
 import java.sql.*;
 import org.json.JSONObject;
 
-//prueba
+//prueba2
 class Worker {
   public static void main(String[] args) {
     try {
