008e96626b5e1205a9171229348cabd20956ba93
micavargas130 <mica.vargas2000@gmail.com>
2021-11-08 17:19:42 -0300

    cambio

--- vote/app.py
+++ vote/app.py
@@ -6,8 +6,8 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "Peliculas")
-option_b = os.getenv('OPTION_B', "Series")
+option_a = os.getenv('OPTION_A', "Motos")
+option_b = os.getenv('OPTION_B', "Autos")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
